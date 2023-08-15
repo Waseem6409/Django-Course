7e98d886135a1b090bbafc316574620838a9af57
Waseem Munir <waseem6409@gmail.com>
2023-08-15 13:26:59 +0500

    delete folder

--- Assignment 2/Showroom/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-from .views import Home, Team, BrandDetail
-
-urlpatterns = [
-    path("", Home, name="Home"),
-    path("team/", Team, name="Team"),
-    path("brand-detail/<int:id>", BrandDetail, name="BrandDetail"),
-]
