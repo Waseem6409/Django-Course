f71afb484edcefa6ba57b8fadcba732b9b2d8c32
Waseem Munir <waseem6409@gmail.com>
2023-08-15 12:47:03 +0500

    Update urls.py

--- Assignment 2/core/urls.py
+++ Assignment 2/core/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("showroom/", include("Showroom.urls"))
+    path("", include("Showroom.urls"))
 ]
 
 if settings.DEBUG:
